421d7ec0c4b88be0db74f8b6ed829e31ed687efb
Alex TSA <alextsa22@gmail.com>
2021-04-23 23:26:54 +0600

    add authentication

--- 16-twitter/main.go
+++ 16-twitter/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 const (
-	pathToEnv = "./16-twitter/.env"
-	consumerKey = "CONSUMER_KEY"
+	pathToEnv      = "./16-twitter/.env"
+	consumerKey    = "CONSUMER_KEY"
 	consumerSecret = "CONSUMER_SECRET"
 )
 
@@ -18,7 +23,39 @@ func main() {
 		log.Fatalf("error loading .env file: %s", err)
 	}
 
-	key := os.Getenv(consumerKey)
-	secret := os.Getenv(consumerSecret)
-	fmt.Println(key, secret)
+	var keys struct {
+		Key    string
+		Secret string
+	}
+	keys.Key = os.Getenv(consumerKey)
+	keys.Secret = os.Getenv(consumerSecret)
+
+	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth(keys.Key, keys.Secret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	var client http.Client
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	var token oauth2.Token
+	dec := json.NewDecoder(res.Body)
+	if err = dec.Decode(&token); err != nil {
+		log.Fatal(err)
+	}
+
+	var conf oauth2.Config
+	tclient := conf.Client(context.Background(), &token)
+	res2, err := tclient.Get("https://api.twitter.com/1.1/statuses/retweets/991053593250758658.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res2.Body.Close()
+	io.Copy(os.Stdout, res2.Body)
 }
