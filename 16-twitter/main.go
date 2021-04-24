5f298588c7f4c84ed64dae3e125c6455afae2504
Alex TSA <alextsa22@gmail.com>
2021-04-24 22:44:59 +0600

    add getting retweeters

--- 16-twitter/main.go
+++ 16-twitter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -52,10 +52,38 @@ func main() {
 
 	var conf oauth2.Config
 	tclient := conf.Client(context.Background(), &token)
-	res2, err := tclient.Get("https://api.twitter.com/1.1/statuses/retweets/991053593250758658.json")
+	usernames, err := retweeters(tclient, "tweetId")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res2.Body.Close()
-	io.Copy(os.Stdout, res2.Body)
+	fmt.Println(usernames)
+}
+
+func retweeters(client *http.Client, tweetId string) ([]string, error) {
+	url := fmt.Sprintf("https://api.twitter.com/1.1/statuses/retweets/%s.json", tweetId)
+
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var retweets []struct {
+		User struct {
+			ScreenName string `json:"screen_name"`
+		} `json:"user"`
+	}
+
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&retweets)
+	if err != nil {
+		return nil, err
+	}
+
+	usernames := make([]string, 0, len(retweets))
+	for _, retweet := range retweets {
+		usernames = append(usernames, retweet.User.ScreenName)
+	}
+
+	return usernames, nil
 }
