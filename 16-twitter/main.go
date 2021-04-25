f5b3731e99b4e96d24bd81874e90750f24766a09
Alex TSA <alextsa22@gmail.com>
2021-04-25 22:17:01 +0600

    add saving usernames

--- 16-twitter/main.go
+++ 16-twitter/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
@@ -13,14 +15,46 @@ import (
 )
 
 const (
-	pathToEnv      = "./16-twitter/.env"
 	consumerKey    = "CONSUMER_KEY"
 	consumerSecret = "CONSUMER_SECRET"
 )
 
 func main() {
-	if err := godotenv.Load(pathToEnv); err != nil {
-		log.Fatalf("error loading .env file: %s", err)
+	var (
+		keyFile   string
+		usersFile string
+		tweetID   string
+	)
+	flag.StringVar(&keyFile, "key", ".env", "the file where you store your consumer key and secret for the twitter api.")
+	flag.StringVar(&usersFile, "users", "users.csv", "the file where users who have retweeted the tweet are stored. this will be created if it does not exist.")
+	flag.StringVar(&tweetID, "tweet", "tweetId", "the id of the Tweet you wish to find retweeters of.")
+	flag.Parse()
+
+	key, secret, err := keys(keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := twitterClient(key, secret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newUsernames, err := retweeters(client, tweetID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	existUsernames := existing(usersFile)
+	allUsernames := merge(newUsernames, existUsernames)
+	if err = writeUsers(usersFile, allUsernames); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func keys(keyFile string) (key, secret string, err error) {
+	if err := godotenv.Load(keyFile); err != nil {
+		return "", "", fmt.Errorf("error loading .env file: %s", err)
 	}
 
 	var keys struct {
@@ -30,33 +64,37 @@ func main() {
 	keys.Key = os.Getenv(consumerKey)
 	keys.Secret = os.Getenv(consumerSecret)
 
-	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	return keys.Key, keys.Secret, nil
+}
+
+func twitterClient(key, secret string) (*http.Client, error) {
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.twitter.com/oauth2/token",
+		strings.NewReader("grant_type=client_credentials"),
+	)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	req.SetBasicAuth(keys.Key, keys.Secret)
+	req.SetBasicAuth(key, secret)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
 
 	var client http.Client
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	var token oauth2.Token
 	dec := json.NewDecoder(res.Body)
-	if err = dec.Decode(&token); err != nil {
-		log.Fatal(err)
-	}
-
-	var conf oauth2.Config
-	tclient := conf.Client(context.Background(), &token)
-	usernames, err := retweeters(tclient, "tweetId")
+	err = dec.Decode(&token)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Println(usernames)
+	var conf oauth2.Config
+
+	return conf.Client(context.Background(), &token), nil
 }
 
 func retweeters(client *http.Client, tweetId string) ([]string, error) {
@@ -86,4 +124,61 @@ func retweeters(client *http.Client, tweetId string) ([]string, error) {
 	}
 
 	return usernames, nil
+}
+
+func existing(usersFile string) []string {
+	f, err := os.Open(usersFile)
+	if err != nil {
+		return []string{}
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	lines, err := r.ReadAll()
+	users := make([]string, 0, len(lines))
+	for _, line := range lines {
+		users = append(users, line[0])
+	}
+
+	return users
+}
+
+func merge(a, b []string) []string {
+	uniq := make(map[string]struct{}, 0)
+	for _, user := range a {
+		uniq[user] = struct{}{}
+	}
+
+	for _, user := range b {
+		uniq[user] = struct{}{}
+	}
+
+	ret := make([]string, 0, len(uniq))
+	for user := range uniq {
+		ret = append(ret, user)
+	}
+
+	return ret
+}
+
+func writeUsers(usersFile string, users []string) error {
+	f, err := os.OpenFile(usersFile, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	for _, user := range users {
+		if err := w.Write([]string{user}); err != nil {
+			return err
+		}
+	}
+
+	w.Flush()
+	if err := w.Error(); err != nil {
+		return err
+	}
+
+	return nil
 }
