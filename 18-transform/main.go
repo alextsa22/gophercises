08417af7fa31757b3957ed06d84a80e263c9cb98
Alex TSA <alextsa22@gmail.com>
2021-05-11 23:54:32 +0600

    add selection

--- 18-transform/main.go
+++ 18-transform/main.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -25,40 +26,100 @@ var (
 
 	imagesHTML = `<html><body>
 			{{range .}}
-				<img src="/{{.}}">
+				<a href="/modify/{{.Name}}?mode={{.Mode}}">
+					<img style="width: 22%;" src="/img/{{.Name}}">
+				</a>
 			{{end}}
 			</body></html>`
 	imagesTpl = template.Must(template.New("").Parse(imagesHTML))
+
+	imagesWithNumShapesHTML = `<html><body>
+			{{range .}}
+				<a href="/modify/{{.Name}}?mode={{.Mode}}&n={{.NumShapes}}">
+					<img style="width: 22%;" src="/img/{{.Name}}">
+				</a>
+			{{end}}
+			</body></html>`
+	imagesWithNumShapesTpl = template.Must(template.New("").Parse(imagesWithNumShapesHTML))
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, uploadForm)
+		if _, err := fmt.Fprint(w, uploadForm); err != nil {
+			log.Printf("fmt.Fptint: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		file, header, err := r.FormFile("image")
 		if err != nil {
-			log.Printf("FormFile(): %v", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			log.Printf("FormFile: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer file.Close()
 
 		ext := filepath.Ext(header.Filename)[1:]
 
-		circleImg, _ := generateImage(file, ext, 10, primitive.ModeCircle)
-		file.Seek(0, 0)
-		rectImg, _ := generateImage(file, ext, 10, primitive.ModeRect)
-		file.Seek(0, 0)
-		polygonImg, _ := generateImage(file, ext, 10, primitive.ModePolygon)
-
-		images := []string{circleImg, rectImg, polygonImg}
-		for i, img := range images {
-			images[i] = strings.ReplaceAll(img, `\`, `/`)
+		onDisk, err := tempFile("", ext)
+		if err != nil {
+			log.Printf("tempFile: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		imagesTpl.Execute(w, images)
+		defer onDisk.Close()
+		if _, err = io.Copy(onDisk, file); err != nil {
+			log.Printf("io.Copy: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/modify/"+filepath.Base(onDisk.Name()), http.StatusFound)
+	})
+	mux.HandleFunc("/modify/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("./img/" + filepath.Base(r.URL.Path))
+		if err != nil {
+			log.Printf("os.Open: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		ext := filepath.Ext(f.Name())[1:]
+
+		modeStr := r.FormValue("mode")
+		if modeStr == "" {
+			renderModeChoices(w, f, ext)
+			return
+		}
+		mode, err := strconv.Atoi(modeStr)
+		if err != nil {
+			log.Printf("strconv.Atoi: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		numShapesStr := r.FormValue("numShapes")
+		if numShapesStr == "" {
+			renderNumShapeChoices(w, f, ext, primitive.Mode(mode))
+			return
+		}
+		numShapes, err := strconv.Atoi(numShapesStr)
+		if err != nil {
+			log.Printf("strconv.Atoi: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_ = numShapes
+
+		if _, err = io.Copy(w, f); err != nil {
+			log.Printf("io.Copy: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 	mux.Handle(
 		"/img/",
@@ -69,15 +130,120 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
+func renderModeChoices(w http.ResponseWriter, rs io.ReadSeeker, ext string) {
+	ns := 10
+	opts := []generateOpts{
+		{NumShapes: ns, Mode: primitive.ModeCircle},
+		{NumShapes: ns, Mode: primitive.ModeRect},
+		{NumShapes: ns, Mode: primitive.ModePolygon},
+		{NumShapes: ns, Mode: primitive.ModeCombo},
+	}
+	images, err := generateImages(rs, ext, opts...)
+	if err != nil {
+		log.Printf("generateImages: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type dataStruct struct {
+		Name string
+		Mode primitive.Mode
+	}
+
+	var data []dataStruct
+	for i, img := range images {
+		data = append(data, dataStruct{
+			Name: filepath.Base(img),
+			Mode: opts[i].Mode,
+		})
+	}
+
+	for _, img := range data {
+		img.Name = strings.ReplaceAll(img.Name, `\`, `/`)
+	}
+
+	if err = imagesTpl.Execute(w, data); err != nil {
+		log.Printf("imagesTpl.Execute: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func renderNumShapeChoices(
+	w http.ResponseWriter,
+	rs io.ReadSeeker,
+	ext string,
+	mode primitive.Mode,
+) {
+	opts := []generateOpts{
+		{NumShapes: 10, Mode: mode},
+		{NumShapes: 20, Mode: mode},
+		{NumShapes: 30, Mode: mode},
+		{NumShapes: 40, Mode: mode},
+	}
+
+	images, err := generateImages(rs, ext, opts...)
+	if err != nil {
+		log.Printf("generateImages: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type dataStruct struct {
+		Name      string
+		NumShapes int
+		Mode      primitive.Mode
+	}
+
+	var data []dataStruct
+	for i, img := range images {
+		data = append(data, dataStruct{
+			Name:      filepath.Base(img),
+			NumShapes: opts[i].NumShapes,
+			Mode:      opts[i].Mode,
+		})
+	}
+
+	for _, img := range data {
+		img.Name = strings.ReplaceAll(img.Name, `\`, `/`)
+	}
+
+	if err = imagesWithNumShapesTpl.Execute(w, data); err != nil {
+		log.Printf("imagesWithNumShapesTpl.Execute: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type generateOpts struct {
+	NumShapes int
+	Mode      primitive.Mode
+}
+
+func generateImages(rs io.ReadSeeker, ext string, opts ...generateOpts) ([]string, error) {
+	var images []string
+	for _, opt := range opts {
+		if _, err := rs.Seek(0, 0); err != nil {
+			return nil, fmt.Errorf("rs.Seek: %v", err)
+		}
+		img, err := generateImage(rs, ext, opt.NumShapes, opt.Mode)
+		if err != nil {
+			return nil, fmt.Errorf("generateImage: %v", err)
+		}
+		images = append(images, img)
+	}
+	return images, nil
+}
+
 func generateImage(r io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
 	out, err := primitive.Transform(r, ext, numShapes, primitive.WithMode(mode))
 	if err != nil {
-		return "", fmt.Errorf("primitive.Transform(): %v", err)
+		return "", fmt.Errorf("primitive.Transform: %v", err)
 	}
 
 	outFile, err := tempFile("", ext)
 	if err != nil {
-		return "", fmt.Errorf("tempFile(); %v", err)
+		return "", fmt.Errorf("tempFile; %v", err)
 	}
 	defer outFile.Close()
 
--- 18-transform/primitive/primitive.go
+++ 18-transform/primitive/primitive.go
@@ -48,28 +48,27 @@ func Transform(image io.Reader, ext string, numShapes int, opts ...func() []stri
 
 	in, err := tempFile("__in_", ext)
 	if err != nil {
-		return nil, fmt.Errorf("tempFile(): failed to create temporary input fule: %v", err)
+		return nil, fmt.Errorf("tempFile: failed to create temporary input fule: %v", err)
 	}
 	defer os.Remove(in.Name())
 	out, err := tempFile("__out_", ext)
 	if err != nil {
-		return nil, fmt.Errorf("tempFile(): failed to create temporary output file: %v", err)
+		return nil, fmt.Errorf("tempFile: failed to create temporary output file: %v", err)
 	}
 	defer os.Remove(out.Name())
 
 	if _, err = io.Copy(in, image); err != nil {
-		return nil, fmt.Errorf("io.Copy(): failed to copy image into temp input file: %v", err)
+		return nil, fmt.Errorf("io.Copy: failed to copy image into temp input file: %v", err)
 	}
 
-	// stdCombo
 	_, err = primitive(in.Name(), out.Name(), numShapes, args...)
 	if err != nil {
-		return nil, fmt.Errorf("primitive(): failed to run the primitive command: %v", err)
+		return nil, fmt.Errorf("primitive: failed to run the primitive command: %v", err)
 	}
 
 	buf := bytes.NewBuffer(nil)
 	if _, err = io.Copy(buf, out); err != nil {
-		return nil, fmt.Errorf("io.Copy(): failed to copy output file into byte buffer: %v", err)
+		return nil, fmt.Errorf("io.Copy: failed to copy output file into byte buffer: %v", err)
 	}
 	return buf, nil
 }
