46e79c1b5c52c9f3393dab2ae4593a51cd6195bb
Alex TSA <alextsa22@gmail.com>
2021-05-12 18:09:46 +0600

    use draw package

--- 19-image/main.go
+++ 19-image/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"image/png"
 	"log"
 	"os"
@@ -15,18 +17,38 @@ func main() {
 	rect := image.Rect(0, 0, width, height)
 	img := image.NewRGBA(rect)
 
+	bg := image.NewUniform(color.RGBA{R: 240, G: 240, B: 240, A: 255})
+
+	draw.Draw(img, rect, bg, image.Point{}, draw.Src)
+
+	mask := image.NewRGBA(image.Rect(0, 0, width, height))
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			img.Set(x, y, color.RGBA{255, 255, 255, 255}) // white
+			var alpha uint8
+			switch {
+			case y < 30:
+				alpha = 255
+			case y < 50:
+				alpha = 100
+			}
+			mask.Set(x, y, color.RGBA{
+				R: uint8((x + y) & 255),
+				G: uint8((x + y) << 1 & 255),
+				B: uint8((x + y) << 2 & 255),
+				A: alpha,
+			})
 		}
 	}
 
 	for i, dp := range data {
-		for x := i*60 + 10; x < (i+1)*60; x++ {
-			for y := height; y >= (100 - dp); y-- {
-				img.Set(x, y, color.RGBA{180, 180, 250, 255})
-			}
-		}
+		x0, y0 := i*60+10, 100-dp
+		x1, y1 := (i+1)*60-1, 100
+		bar := image.Rect(x0, y0, x1, y1)
+		grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
+		draw.Draw(img, bar, grey, image.Point{}, draw.Src)
+
+		red := image.NewUniform(color.RGBA{R: 255, G: 0, B: 0, A: 255})
+		draw.DrawMask(img, bar, red, image.Point{}, mask, image.Point{X: x0, Y: y0}, draw.Over)
 	}
 
 	f, err := os.Create("demo.png")
@@ -38,4 +60,6 @@ func main() {
 	if err = png.Encode(f, img); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("image created.")
 }
