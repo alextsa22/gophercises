16b028d2dfce1b8cfb647f15b8e61a48850fe90f
Alex TSA <alextsa22@gmail.com>
2021-05-13 23:32:13 +0600

    use svgo pkg & add demo.svg

--- 19-image/main.go
+++ 19-image/main.go
@@ -2,64 +2,65 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
 	"log"
 	"os"
+
+	svg "github.com/ajstarks/svgo"
 )
 
 func main() {
-	data := []int{100, 33, 73, 64}
-
-	width, height := len(data)*60+10, 100
-	rect := image.Rect(0, 0, width, height)
-	img := image.NewRGBA(rect)
-
-	bg := image.NewUniform(color.RGBA{R: 240, G: 240, B: 240, A: 255})
-
-	draw.Draw(img, rect, bg, image.Point{}, draw.Src)
-
-	mask := image.NewRGBA(image.Rect(0, 0, width, height))
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			var alpha uint8
-			switch {
-			case y < 30:
-				alpha = 255
-			case y < 50:
-				alpha = 100
-			}
-			mask.Set(x, y, color.RGBA{
-				R: uint8((x + y) & 255),
-				G: uint8((x + y) << 1 & 255),
-				B: uint8((x + y) << 2 & 255),
-				A: alpha,
-			})
-		}
-	}
-
-	for i, dp := range data {
-		x0, y0 := i*60+10, 100-dp
-		x1, y1 := (i+1)*60-1, 100
-		bar := image.Rect(x0, y0, x1, y1)
-		grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
-		draw.Draw(img, bar, grey, image.Point{}, draw.Src)
-
-		red := image.NewUniform(color.RGBA{R: 255, G: 0, B: 0, A: 255})
-		draw.DrawMask(img, bar, red, image.Point{}, mask, image.Point{X: x0, Y: y0}, draw.Over)
-	}
-
-	f, err := os.Create("demo.png")
+	f, err := os.OpenFile("demo.svg", os.O_CREATE, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	if err = png.Encode(f, img); err != nil {
-		log.Fatal(err)
+	data := []struct {
+		Month string
+		Point int
+	}{
+		{Month: "Jan", Point: 171},
+		{Month: "Feb", Point: 180},
+		{Month: "Mar", Point: 100},
+		{Month: "Apr", Point: 87},
+		{Month: "May", Point: 66},
+		{Month: "Jun", Point: 40},
+		{Month: "Jul", Point: 32},
+		{Month: "Aug", Point: 55},
+		{Month: "Sep", Point: 0},
+		{Month: "Oct", Point: 0},
+		{Month: "Nov", Point: 0},
+		{Month: "Dec", Point: 0},
 	}
+
+	canvas := svg.New(f)
+	width, height := len(data)*60+10, 300
+	threshold := 120
+
+	max := 0
+	for _, dp := range data {
+		if max < dp.Point {
+			max = dp.Point
+		}
+	}
+
+	canvas.Start(width, height)
+	canvas.Rect(0, 0, width, height, "fill:rgb(255, 255, 255)")
+	for i, dp := range data {
+		percent := dp.Point * (height - 50) / max
+		canvas.Rect(i*60+10, (height-50)-percent, 50, percent,
+			"fill:rgb(77, 200, 232)")
+		canvas.Text(i*60+35, height-20, dp.Month,
+			"font-size:14pt;fill:rgb(150, 150, 150);text-anchor:middle")
+	}
+	threshPercent := threshold * (height - 50) / max
+	canvas.Line(0, height-threshPercent, width, height-threshPercent,
+		"stroke:rgb(255, 100, 100);opacity:0.8;stroke-width:2")
+	canvas.Rect(0, 0, width, height-threshPercent,
+		"fill:rgb(255, 100, 100);opacity:0.2")
+	canvas.Line(0, height-50, width, height-50,
+		"stroke:rgb(150, 150, 150);stroke-width:2")
+	canvas.End()
 
 	fmt.Println("image created.")
 }
